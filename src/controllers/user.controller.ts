088e667f62c98111aa48593920bfafcf08f32614
mdalaminfaraji <alaminice1617@gmail.com>
2023-11-23 18:16:03 +0600

    user services and controller done

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -17,6 +17,93 @@ const createUser = async (req: Request, res: Response) => {
     });
   }
 };
+
+const getAllUser = async (req: Request, res: Response) => {
+  try {
+    const result = await userServices.getAllUser();
+    res.status(200).json({
+      success: true,
+      message: 'Users fetched successfully!',
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message || 'Something went wrong',
+    });
+  }
+};
+
+const getSingleUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const result = await userServices.getSingleUser(id);
+    res.status(200).json({
+      success: true,
+      message: 'User fetched successfully!',
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      success: false,
+      message: 'User not found',
+      error: {
+        code: 404,
+        description: 'User not found!',
+      },
+    });
+  }
+};
+
+const updateUser = async (req: Request, res: Response) => {
+  try {
+    const updatedData = req.body;
+    const { id } = req.params;
+
+    const result = await userServices.updateUser(id, updatedData);
+    res.status(201).json({
+      success: true,
+      message: 'User updated successfully!',
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      success: false,
+      message: 'User not found',
+      error: {
+        code: 404,
+        description: 'User not found!',
+      },
+    });
+  }
+};
+
+const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    await userServices.deleteUser(id);
+    res.status(201).json({
+      success: true,
+      message: 'User deleted successfully!',
+      data: null,
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      success: false,
+      message: 'User not found',
+      error: {
+        code: 404,
+        description: 'User not found!',
+      },
+    });
+  }
+};
 export const userController = {
   createUser,
+  getAllUser,
+  getSingleUser,
+  updateUser,
+  deleteUser,
 };
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -6,6 +6,36 @@ const createUser = async (userData: IUsers): Promise<IUsers> => {
   return result;
 };
 
+const getAllUser = async (): Promise<IUsers[]> => {
+  const result = await User.find();
+  return result;
+};
+
+const getSingleUser = async (id: string): Promise<IUsers | null> => {
+  const result = await User.findById(id);
+  return result;
+};
+
+const updateUser = async (
+  id: string,
+  updatedData: IUsers
+): Promise<IUsers | null> => {
+  const result = await User.findByIdAndUpdate(id, updatedData, {
+    new: true,
+    runValidators: true,
+  });
+  return result;
+};
+
+const deleteUser = async (id: string): Promise<IUsers | null> => {
+  const result = await User.findByIdAndDelete(id);
+  return result;
+};
+
 export const userServices = {
   createUser,
+  getAllUser,
+  getSingleUser,
+  updateUser,
+  deleteUser,
 };
